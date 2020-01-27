1d73cb9518a6fd15a21f7c94e6d348423bca0ce1
Randrita <randritas@gmail.com>
2020-01-27 23:19:42 +0530

    additon of two numbers

---
+++ Randrita/addition.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+int a,b,c;
+cout<<"enter the first number"<<endl;
+cin>>a;
+cout<<"enter the second number"<<endl;
+cin>>b;
+c=a+b;
+cout<<"the sum is"<<c;
+ return 0;}
